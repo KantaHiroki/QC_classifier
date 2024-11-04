72d3a7d15561cb8137f7c13221ae92d3abb2f571
KantaHiroki <1519094@ed.tus.ac.jp>
2024-11-04 18:32:53 +0900

    update

--- clasification.py
+++ clasification.py
@@ -2,15 +2,11 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
-import sys
 import numpy as np
-import random
 import glob
-import math
 import tqdm
 #import generator as gen
 import tensorflow as tf
-import keras
 import matplotlib.pyplot as plt
 from curses import tparm
 from fnmatch import fnmatch
@@ -19,10 +15,7 @@ from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
 import seaborn as sns
 import pandas as pd
 import codecs
-import generator
-import pxrd
 from mylib import save_fig
-import evaluation
 
 os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
@@ -30,243 +23,13 @@ dic_wvl = {}
 dic_wvl['Cu_Ka'] = 1.54059
 
 
-def parameters(flag, aico_min, aico_max, tth_min, tth_max):
-  if flag == 'tth':
-    return tth_min, tth_max, 0.01, 0.01, 0.1
-  # elif flag == 'Qrlu':
-  #   xaxis_min = calc_scattering(aico_max, tth_min, 1.54059)
-  #   xaxis_max = calc_scattering(aico_min, tth_max, 1.54059)
-  #   xaxis_step = (xaxis_max-xaxis_min)/6000
-  #   return xaxis_min, xaxis_max, xaxis_step, 0.001, 0.01
 
-def result(predicts):
-    QC_num = 0
-    AC11_num = 0
-    AC21_num = 0
-    Others_num = 0
-    for value in predicts:
-        max_value = max(value)
-        max_index = np.argmax(value)
-        if max_index == 0:
-            QC_num += 1
-        elif max_index == 1:
-            AC11_num += 1
-        elif max_index == 2:
-            AC21_num += 1
-        elif max_index == 3:
-            Others_num += 1
-    return [QC_num, AC11_num, AC21_num, Others_num]
 
-def confusion_matrix(QC, AC11, AC21, Others, outputpath, labels):
-    cm=np.array([[QC[0], QC[1], QC[2], QC[3]], [AC11[0], AC11[1], AC11[2], AC11[3]], [AC21[0], AC21[1], AC21[2], AC21[3]], [Others[0], Others[1], Others[2], Others[3]]])
-    # plot the results
-    fig, (ax1) = plt.subplots(1,  figsize=(13, 5), dpi=150)
-    cmd = ConfusionMatrixDisplay(cm,display_labels=labels)
-    cmd.plot(
-        cmap=plt.cm.Blues,
-        ax=ax1
-        )
-    plt.show()
-    fig.savefig(outputpath)
-    return
 
-def read_pattern_in_file(path_exptdata, file_path):
-    tth_list = []
-    Intensity_list = []
-    try:
-        # f = open(file_path, 'r', encoding="utf-8")
-        with codecs.open(file_path, 'r', encoding="utf-8") as f:
-            lines = f.readlines()
-    except:
-        # f = open(file_path, 'r', encoding="shift-jis")
-        with codecs.open(file_path, 'r', encoding="shift-jis") as f:
-            lines = f.readlines()
-    for line in lines:
-        if line[0] == '*':
-            continue
-        elif line[0]=='#':
-            continue
-        line_list = line.split()
-        try:
-            tth = float(line_list[0])
-        except:
-            continue
-        if 20 <= tth < 80:
-            tth_list.append(tth)
-            Intensity = float(line_list[1])
-            Intensity_list.append(Intensity)
-    f.close()
 
-    # print(file_path, len(Intensity_list))
-    if len(Intensity_list)<6000:
-        tth_interval = tth_list[1]-tth_list[0]
-        Intensity_list = data_compensate(tth_list, Intensity_list, tth_interval)
-    # if len(Intensity_list)!=6000:
-    #     return 'error1'
-    return Intensity_list
 
-def data_download(path_exptdata, extension):
-    files = {}
-    extension_list = [extension, extension.upper()]
-    for ext in extension_list:
-        file_list = glob.glob(path_exptdata+'*'+ext)
-        len_file_list = len(file_list)
-        count_error = 0
-        for file_name in tqdm.tqdm(file_list[:], desc='Data Download'):
-            # try:
-                Intensity_list = read_pattern_in_file(path_exptdata, file_name)
-                if Intensity_list=='error1':
-                    print("="*80)
-                    print('Error! Please check data length, expect 6000 (%s)'%(file_name))
-                    print("="*80)
-                    continue
-                x_test = np.array([Intensity_list], np.float64)
-                x_test = x_test-np.min(x_test, axis = 1).reshape(1, 1)
-                x_test = x_test/np.max(x_test, axis = 1).reshape(1, 1)
-                tf.keras.backend.set_floatx('float64')
-                x_test_ = x_test[..., tf.newaxis]
-                files[file_name] = x_test_
-            # except:
-            #     print("=============================")
-            #     print('Error! Please check the file.')
-            #     print(file_name[len(path_exptdata):])
-            #     print("=============================")
-            #     file_list.remove(file_name)
-            #     count_error += 1
-            #     print(count_error, '/', len_file_list, ', error / file')
-    return files
 
-def data_download_from_list(path_exptdir, path_file_list):
-    files = {}
-    count_error = 0
-    file_list = open(path_file_list, 'r', encoding="utf-8")
-    # print(path_exptdir+file_list.readlines()[1][:-1])
-    # len_file_list = len(file_list.readlines())
-    for file_name in file_list.readlines():
-        try:
-            tth_list = []
-            Intensity_list = []
-            # print(path_exptdir+file_name[:-1])
-            f = open(path_exptdir+file_name[:-1], 'r', encoding="shift-jis")
-            #f = open(file_name, 'r')
-            #print(f.readlines()[300])
-            for line in f.readlines():
-                # try:
-                    if line[0] == '*':
-                        continue
-                    elif line[0]=='#':
-                        continue
-                    line_list = line[:-1].split()
-                    tth = float(line_list[0])
-                    if 20 <= tth < 80:
-                        tth_list.append(tth)
-                        Intensity = float(line_list[1])
-                        Intensity_list.append(Intensity)
-                #except:
-                #    pass
-            f.close()
 
-            if len(Intensity_list)!=6000:
-                print("=============================")
-                print('Error! Please check the file. (%s)'%(file_name))
-                print("=============================")
-                # file_list.remove(file_name)
-                # count_error += 1
-                # print(count_error, '/', len_file_list, ', error / file')
-                # tth_interval = tth_list[1]-tth_list[0]
-                # Intensity_list = data_compensate(tth_list, Intensity_list, tth_interval)
-
-            x_test = np.array([Intensity_list], np.float64)
-            x_test = x_test-np.min(x_test, axis = 1).reshape(1, 1)
-            x_test = x_test/np.max(x_test, axis = 1).reshape(1, 1)
-            tf.keras.backend.set_floatx('float64')
-            x_test_ = x_test[..., tf.newaxis]
-            files[file_name] = x_test_
-        except:
-            print("=============================")
-            print('Error! Please check the file.')
-            print(file_name)
-            print("=============================")
-            # file_list.remove(file_name)
-            # count_error += 1
-            # print(count_error, '/', len_file_list, ', error / file')
-    file_list.close()
-    return files
-
-def data_compensate(tth_list, Intensity_list, tth_interval):
-    tth_len = 6000
-    tth_len_ = int((80.0-20.0)/tth_interval)
-    step = int(tth_len/tth_len_)
-    pattern_list = []
-    for i in range(len(tth_list)):
-        intensity1 = Intensity_list[i]
-        pattern_list.append(intensity1)
-        if i==len(tth_list)-1:
-            intensity2 = Intensity_list[i]
-        else:
-            intensity2 = Intensity_list[i+1]
-            intensity = (intensity1+intensity2)/step
-        pattern_list.append(intensity)
-    return pattern_list
-
-def get_TPTNFPFN(QC_prediction_list, AC11_prediction_list, AC21_prediction_list, Others_prediction_list, data_num, label):
-    if label=='QC':
-        TP = QC_prediction_list[0]
-        FP = AC11_prediction_list[0]+AC21_prediction_list[0]+Others_prediction_list[0]
-        FN = QC_prediction_list[1]+QC_prediction_list[2]+QC_prediction_list[3]
-        TN = data_num-(TP+FP+FN)
-    if label=='AC11':
-        TP = AC11_prediction_list[1]
-        FP = QC_prediction_list[1]+AC21_prediction_list[1]+QC_prediction_list[1]
-        FN = AC11_prediction_list[0]+AC11_prediction_list[2]+AC11_prediction_list[3]
-        TN = data_num-(TP+FP+FN)
-    if label=='AC21':
-        TP = AC21_prediction_list[2]
-        FP = QC_prediction_list[2]+AC11_prediction_list[2]+Others_prediction_list[2]
-        FN = AC21_prediction_list[0]+AC21_prediction_list[1]+AC21_prediction_list[3]
-        TN = data_num-(TP+FP+FN)
-    if label=='Others':
-        TP = Others_prediction_list[3]
-        FP = QC_prediction_list[3]+AC11_prediction_list[3]+AC21_prediction_list[3]
-        FN = Others_prediction_list[0]+Others_prediction_list[1]+Others_prediction_list[2]
-        TN = data_num-(TP+FP+FN)
-    return TP, FP, FN, TN
-
-def calc_accuracy(TP_all, data_num):
-    accuracy = TP_all/data_num
-    return round(accuracy,4)
-
-def calc_recall(TP, FN):
-    recall = TP/(TP+FN)
-    return recall
-
-def calc_precision(TP, FP):
-    precision = TP/(TP+FP)
-    return precision
-
-def calc_F1(recall, precision):
-    F1_value = (2*recall*precision)/(recall+precision)
-    return F1_value
-
-def calc_evaluation(TP, FP, FN, TN):
-    accuracy = calc_accuracy(TP, TP+FP+FN+TN)
-    recall = calc_recall(TP, FN)
-    precision = calc_precision(TP, FP)
-    F1_value = calc_F1(recall, precision)
-    return round(accuracy,4), round(recall,4), round(precision,4), round(F1_value,4)
-
-def TablePlot(df,outputPath,w,h):
-    fig, ax = plt.subplots(figsize=(w,h))
-    ax.axis('off')
-    table = ax.table(cellText=df.values,
-            colLabels=df.columns,
-            rowLabels=df.index,
-            loc='center',
-            bbox=[0,0,1,1])
-    table.auto_set_font_size(False)
-    table.set_fontsize(14)
-    plt.savefig(outputPath)
-    return
 
 def run(path_model, expt_data_path, extension, output_path, xaxis_min, xaxis_max, xaxis_step, flag_plot):
     """
--- mylib/read_file.py
+++ mylib/read_file.py
@@ -19,7 +19,8 @@ def data_compensate(tth_list, Intensity_list, tth_interval):
         pattern_list.append(intensity)
     return pattern_list
 
-def read_pattern_in_file(file_path):
+
+def read_pattern_in_file(path_exptdata, file_path):
     tth_list = []
     Intensity_list = []
     try:
@@ -30,13 +31,12 @@ def read_pattern_in_file(file_path):
         # f = open(file_path, 'r', encoding="shift-jis")
         with codecs.open(file_path, 'r', encoding="shift-jis") as f:
             lines = f.readlines()
-    # try:
     for line in lines:
-        if '*' in line:
+        if line[0] == '*':
             continue
-        elif '#' in line:
+        elif line[0]=='#':
             continue
-        line_list = line[:-1].split(' ')
+        line_list = line.split()
         try:
             tth = float(line_list[0])
         except:
@@ -45,13 +45,122 @@ def read_pattern_in_file(file_path):
             tth_list.append(tth)
             Intensity = float(line_list[1])
             Intensity_list.append(Intensity)
-    if len(Intensity_list)!=6000:
-        print('Warning : Not appropriate 2θ step.')
+    f.close()
+
+    # print(file_path, len(Intensity_list))
+    if len(Intensity_list)<6000:
         tth_interval = tth_list[1]-tth_list[0]
         Intensity_list = data_compensate(tth_list, Intensity_list, tth_interval)
-    # except:
-        # return print('Error : Unknown problem.')
+    # if len(Intensity_list)!=6000:
+    #     return 'error1'
     return Intensity_list
+
+
+def data_download_from_list(path_exptdir, path_file_list):
+    files = {}
+    count_error = 0
+    file_list = open(path_file_list, 'r', encoding="utf-8")
+    # print(path_exptdir+file_list.readlines()[1][:-1])
+    # len_file_list = len(file_list.readlines())
+    for file_name in file_list.readlines():
+        try:
+            tth_list = []
+            Intensity_list = []
+            # print(path_exptdir+file_name[:-1])
+            f = open(path_exptdir+file_name[:-1], 'r', encoding="shift-jis")
+            #f = open(file_name, 'r')
+            #print(f.readlines()[300])
+            for line in f.readlines():
+                # try:
+                    if line[0] == '*':
+                        continue
+                    elif line[0]=='#':
+                        continue
+                    line_list = line[:-1].split()
+                    tth = float(line_list[0])
+                    if 20 <= tth < 80:
+                        tth_list.append(tth)
+                        Intensity = float(line_list[1])
+                        Intensity_list.append(Intensity)
+                #except:
+                #    pass
+            f.close()
+
+            if len(Intensity_list)!=6000:
+                print("=============================")
+                print('Error! Please check the file. (%s)'%(file_name))
+                print("=============================")
+                # file_list.remove(file_name)
+                # count_error += 1
+                # print(count_error, '/', len_file_list, ', error / file')
+                # tth_interval = tth_list[1]-tth_list[0]
+                # Intensity_list = data_compensate(tth_list, Intensity_list, tth_interval)
+
+            x_test = np.array([Intensity_list], np.float64)
+            x_test = x_test-np.min(x_test, axis = 1).reshape(1, 1)
+            x_test = x_test/np.max(x_test, axis = 1).reshape(1, 1)
+            tf.keras.backend.set_floatx('float64')
+            x_test_ = x_test[..., tf.newaxis]
+            files[file_name] = x_test_
+        except:
+            print("=============================")
+            print('Error! Please check the file.')
+            print(file_name)
+            print("=============================")
+            # file_list.remove(file_name)
+            # count_error += 1
+            # print(count_error, '/', len_file_list, ', error / file')
+    file_list.close()
+    return files
+
+def data_download(path_exptdata, extension):
+    files = {}
+    extension_list = [extension, extension.upper()]
+    for ext in extension_list:
+        file_list = glob.glob(path_exptdata+'*'+ext)
+        len_file_list = len(file_list)
+        count_error = 0
+        for file_name in tqdm.tqdm(file_list[:], desc='Data Download'):
+            # try:
+                Intensity_list = read_pattern_in_file(path_exptdata, file_name)
+                if Intensity_list=='error1':
+                    print("="*80)
+                    print('Error! Please check data length, expect 6000 (%s)'%(file_name))
+                    print("="*80)
+                    continue
+                x_test = np.array([Intensity_list], np.float64)
+                x_test = x_test-np.min(x_test, axis = 1).reshape(1, 1)
+                x_test = x_test/np.max(x_test, axis = 1).reshape(1, 1)
+                tf.keras.backend.set_floatx('float64')
+                x_test_ = x_test[..., tf.newaxis]
+                files[file_name] = x_test_
+            # except:
+            #     print("=============================")
+            #     print('Error! Please check the file.')
+            #     print(file_name[len(path_exptdata):])
+            #     print("=============================")
+            #     file_list.remove(file_name)
+            #     count_error += 1
+            #     print(count_error, '/', len_file_list, ', error / file')
+    return files
+
+def result(predicts):
+    QC_num = 0
+    AC11_num = 0
+    AC21_num = 0
+    Others_num = 0
+    for value in predicts:
+        max_value = max(value)
+        max_index = np.argmax(value)
+        if max_index == 0:
+            QC_num += 1
+        elif max_index == 1:
+            AC11_num += 1
+        elif max_index == 2:
+            AC21_num += 1
+        elif max_index == 3:
+            Others_num += 1
+    return [QC_num, AC11_num, AC21_num, Others_num]
 
 def preprocess_pattern(Intensity_list):
     x_test = np.array([Intensity_list], np.float64)
@@ -60,3 +169,62 @@ def preprocess_pattern(Intensity_list):
     tf.keras.backend.set_floatx('float64')
     x_test_ = x_test[..., tf.newaxis]
     return x_test_
+
+def get_TPTNFPFN(QC_prediction_list, AC11_prediction_list, AC21_prediction_list, Others_prediction_list, data_num, label):
+    if label=='QC':
+        TP = QC_prediction_list[0]
+        FP = AC11_prediction_list[0]+AC21_prediction_list[0]+Others_prediction_list[0]
+        FN = QC_prediction_list[1]+QC_prediction_list[2]+QC_prediction_list[3]
+        TN = data_num-(TP+FP+FN)
+    if label=='AC11':
+        TP = AC11_prediction_list[1]
+        FP = QC_prediction_list[1]+AC21_prediction_list[1]+QC_prediction_list[1]
+        FN = AC11_prediction_list[0]+AC11_prediction_list[2]+AC11_prediction_list[3]
+        TN = data_num-(TP+FP+FN)
+    if label=='AC21':
+        TP = AC21_prediction_list[2]
+        FP = QC_prediction_list[2]+AC11_prediction_list[2]+Others_prediction_list[2]
+        FN = AC21_prediction_list[0]+AC21_prediction_list[1]+AC21_prediction_list[3]
+        TN = data_num-(TP+FP+FN)
+    if label=='Others':
+        TP = Others_prediction_list[3]
+        FP = QC_prediction_list[3]+AC11_prediction_list[3]+AC21_prediction_list[3]
+        FN = Others_prediction_list[0]+Others_prediction_list[1]+Others_prediction_list[2]
+        TN = data_num-(TP+FP+FN)
+    return TP, FP, FN, TN
+
+def calc_accuracy(TP_all, data_num):
+    accuracy = TP_all/data_num
+    return round(accuracy,4)
+
+def calc_recall(TP, FN):
+    recall = TP/(TP+FN)
+    return recall
+
+def calc_precision(TP, FP):
+    precision = TP/(TP+FP)
+    return precision
+
+def calc_F1(recall, precision):
+    F1_value = (2*recall*precision)/(recall+precision)
+    return F1_value
+
+def calc_evaluation(TP, FP, FN, TN):
+    accuracy = calc_accuracy(TP, TP+FP+FN+TN)
+    recall = calc_recall(TP, FN)
+    precision = calc_precision(TP, FP)
+    F1_value = calc_F1(recall, precision)
+    return round(accuracy,4), round(recall,4), round(precision,4), round(F1_value,4)
+
+def TablePlot(df,outputPath,w,h):
+    fig, ax = plt.subplots(figsize=(w,h))
+    ax.axis('off')
+    table = ax.table(cellText=df.values,
+            colLabels=df.columns,
+            rowLabels=df.index,
+            loc='center',
+            bbox=[0,0,1,1])
+    table.auto_set_font_size(False)
+    table.set_fontsize(14)
+    plt.savefig(outputPath)
+    return
